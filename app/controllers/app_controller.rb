c04b2784607c742645844c5f402bf8ab8daa1784
github username <github email address>
2017-11-01 17:54:09 -0500

    creating separate controller and stubbing out signup form

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -4,6 +4,27 @@ class AppController < Sinatra::Base
     enable :sessions
     set :session_secret, "secret"
   end
-  
+
+  get '/' do
+    erb :home
+  end
+
+  get '/signup' do
+    if logged_in?
+      redirect '/spices'
+    else
+      erb :"users/signup"
+    end
+  end
+
+  helpers do
+   def logged_in?
+     !!session[:user_id]
+   end
+
+   def current_user
+     User.find(session[:user_id])
+   end
+  end
 
 end
