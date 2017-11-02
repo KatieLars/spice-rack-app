3a7cb41c034b3b158b0c5b755e8f640b2e9fbd70
github username <github email address>
2017-11-02 17:30:37 -0500

    Reconfiguring views and testing safeguards for login (no blanks and user authentication)

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -8,25 +8,28 @@ class AppController < Sinatra::Base
     register Sinatra::Flash
   end
 
-  get '/' do
+  get '/' do #done
     erb :home
   end
 
-  get '/login' do
-    if !logged_in?
-      erb :login
-    else
+  get '/login' do #if user not already logged in, displays login form
+    if logged_in?
       @user = current_user
       redirect "/#{@user.slug}/home"
+    else
+      erb :login
     end
   end
 
   post '/login' do
     user = User.find_by(:username => params[:username])
-    if user && user.authenticate(params[:password])
+    if empty_field = params.detect {|k, v| v.empty?}[0].to_s
+      flash[:warning] = "Please enter #{empty_field}"
+      redirect '/login'
+    elsif user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect "/#{user.slug}/home"
-    else
+    else #goes to sign up if can't find/authenticate user
       redirect "/signup"
     end
   end
@@ -36,7 +39,7 @@ class AppController < Sinatra::Base
       @user = current_user
       redirect "/#{@user.slug}/home"
     else
-      erb :"users/signup"
+      erb :signup
     end
   end
 
