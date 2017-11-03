003ab35478ddc7ac44e08eb50255d72e02c61289
github username <github email address>
2017-11-03 17:23:58 -0500

    finishing touches to seeds

--- lib/flavor_parser.rb
+++ lib/flavor_parser.rb
@@ -1,15 +1,14 @@
 class FlavorParser
 
   def self.call
-    self.new.class_builder(file)
+    self.new.class_builder
   end
 
   def file #reads file data, spits out an array
     File.read("./db/data/data.db").gsub("\n", "").split(",")
   end
 
-  def class_builder(file) #builds classes
+  def class_builder
     file.collect {|flavor| Flavor.find_or_create_by(name: flavor)}
   end
-
 end
