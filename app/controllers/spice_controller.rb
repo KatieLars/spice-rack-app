471965850e764d961e973faf8a6189ccb310f024
github username <github email address>
2017-11-03 17:21:23 -0500

    Seeding flavors!

--- app/controllers/spice_controller.rb
+++ app/controllers/spice_controller.rb
@@ -10,7 +10,7 @@ class SpiceController < AppController
     end
   end
 
-  get '/spices/flavors' do #will this get all spice from any user or just the spices from the user profile?
+  get '/spices/flavors' do #index by flavor
     @user = current_user
     if current_user
       @flavors = current_user.flavors
@@ -20,7 +20,21 @@ class SpiceController < AppController
     end
   end
 
-  
+get '/spices/:slug' do #show
+  @spice = Spice.find_by_slug(params[:slug])
+  if current_user
+    erb :"spices/show"
+  else
+    redirect '/login'
+  end
+end
 
+get '/spices/new' do #new spice form
+  if current_user
+    erb :"spices/new"
+  else
+    redirect '/login'
+  end
+end
 
 end
--- lib/flavor_parser.rb
+++ lib/flavor_parser.rb
@@ -1,7 +1,15 @@
 class FlavorParser
 
-  def class_builder(flavor_list)
-    flavor_list.each {|flavor| flavor = Flavor.find_or_create_by(name: flavor)}
+  def self.call
+    self.new.class_builder(file)
   end
-  
+
+  def file #reads file data, spits out an array
+    File.read("./db/data/data.db").gsub("\n", "").split(",")
+  end
+
+  def class_builder(file) #builds classes
+    file.collect {|flavor| Flavor.find_or_create_by(name: flavor)}
+  end
+
 end
