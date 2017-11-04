fb4219ba9941c634ccd10b342976afbd9a9902e7
github username <github email address>
2017-11-03 20:58:08 -0500

    Working on show and making sure selected spice belongs to the current user

--- app/controllers/spice_controller.rb
+++ app/controllers/spice_controller.rb
@@ -21,10 +21,16 @@ class SpiceController < AppController
   end
 
 get '/spices/:slug' do #show
-  @spice = Spice.find_by_slug(params[:slug])
-  if current_user
+  slug_spice = Spice.find_by_slug(:slug)
+  customer_spice = Spice.find_by(:user_id => session[:user_id])
+  if current_user && (slug_spice == customer_spice) #if the spice found by the slug and by the id is the same
+    #sets the spice to an instance variable
+    @spice = slug_spice
     erb :"spices/show"
-  else
+  elsif current_user && !(slug_spice == customer_spice)
+    #if customer logged in, but the slug spice is not in his/her rack
+    flash[:warning] = "This spice is not in your rack."
+  else #if user not logged in
     redirect '/login'
   end
 end
