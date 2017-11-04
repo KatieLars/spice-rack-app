8831535f402a73907b8281399b5b03992339562d
github username <github email address>
2017-11-04 10:50:30 -0500

    Changed relationship between flavors and users to has_many-has_many

--- app/models/flavor.rb
+++ app/models/flavor.rb
@@ -1,6 +1,6 @@
 class Flavor < ActiveRecord::Base
   has_many :spices
   has_many :recipes, through: :recipe_spice_flavor
-  belongs_to :user
+  has_many :users, through: :recipe_spice_flavor
   validates_presence_of :name
 end
