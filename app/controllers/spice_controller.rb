cae762095853d9c10e411996a965e9e9de8300a6
github username <github email address>
2017-11-06 10:23:10 -0600

    debugging styled flash

--- app/controllers/spice_controller.rb
+++ app/controllers/spice_controller.rb
@@ -6,7 +6,8 @@ class SpiceController < AppController
       @spices = @user.spices
       erb :"spices/index"
     elsif current_user && @user.spices.none?
-      flash[:notification] = "Your spice rack is empty"
+      binding.pry
+      flash[:empty_notification] = "Your spice rack is empty" #flash notification not working
       erb :"spices/index"
     else
       redirect "/"
@@ -33,8 +34,10 @@ get '/spices/new' do #new spice form
 end
 
 post '/spices/new' do #should create a new spice and recipe
-  spice = Spice.create(params[:spice])
+  spice = Spice.new(params[:spice])
   if current_user
+    spice.user_id = session[:user_id]
+    spice.save
     Recipe.create(params[:recipe])
     redirect "/spices/#{spice.slug}"
   else
@@ -43,7 +46,6 @@ post '/spices/new' do #should create a new spice and recipe
 end
 
 get '/spices/:slug' do #show
-
   slug_spice = Spice.find_by_slug(params[:slug])
   customer_spice = Spice.find_by(:user_id => session[:user_id])
   if current_user && (slug_spice == customer_spice) #if the spice found by the slug and by the id is the same
@@ -51,8 +53,10 @@ get '/spices/:slug' do #show
     @spice = slug_spice
     erb :"spices/show"
   elsif current_user && !(slug_spice == customer_spice)
+    binding.pry
     #if customer logged in, but the slug spice is not in his/her rack
-    flash[:warning] = "This spice is not in your rack."
+    flash[:spice_warning] = "This spice is not in your rack."
+    erb :"spices/show"
   else #if user not logged in
     redirect '/login'
   end
