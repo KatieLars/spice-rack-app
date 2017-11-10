16e34bc478f844fc195e4ac7468267d79073cccd
github username <github email address>
2017-11-09 21:04:54 -0600

    changed string to URI

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@
 require 'bundler/setup'
 Bundler.require
 
-DATABASE_URL="postgres://ahddyelhqcrghk:a9890d2747b5a132ba2f80cf891b6e851f6c0113d9f24e65abdf0ad998940b73@ec2-50-17-235-5.compute-1.amazonaws.com:5432/d3n9tu2hps02jt"
+DATABASE_URL=URI.parse("postgres://ahddyelhqcrghk:a9890d2747b5a132ba2f80cf891b6e851f6c0113d9f24e65abdf0ad998940b73@ec2-50-17-235-5.compute-1.amazonaws.com:5432/d3n9tu2hps02jt")
 
 ActiveRecord::Base.establish_connection(
   :adapter => "postgresql",
