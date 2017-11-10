97e9cbb8f0d2eace1101a922ed363e96135dcd9b
github username <github email address>
2017-11-09 18:09:19 -0600

    connecting to database

--- config/environment.rb
+++ config/environment.rb
@@ -3,10 +3,11 @@
 require 'bundler/setup'
 Bundler.require
 
-#ActiveRecord::Base.establish_connection(
-#  :adapter => "postgresql",
-#  :database => "db/spice_rack.sqlite"
-#)
+db = URI.parse('postgres://localhost/spice_rack')
+ActiveRecord::Base.establish_connection(
+  :adapter => "postgresql",
+  :database => db.path[1..-1]
+)
 
 require_all 'app'
 require_all 'lib'
