fac0fc777e42137e5dbc5c9b8d997b25c4ab21c6
github username <github email address>
2017-11-09 21:31:23 -0600

    Going back to sqlite

--- config/environment.rb
+++ config/environment.rb
@@ -3,11 +3,11 @@
 require 'bundler/setup'
 Bundler.require
 
-DATABASE_URL=URI.parse("postgres://ahddyelhqcrghk:a9890d2747b5a132ba2f80cf891b6e851f6c0113d9f24e65abdf0ad998940b73@ec2-50-17-235-5.compute-1.amazonaws.com:5432/d3n9tu2hps02jt")
+#DATABASE_URL=URI.parse("postgres://ahddyelhqcrghk:a9890d2747b5a132ba2f80cf891b6e851f6c0113d9f24e65abdf0ad998940b73@ec2-50-17-235-5.compute-1.amazonaws.com:5432/d3n9tu2hps02jt")
 
 ActiveRecord::Base.establish_connection(
   :adapter => "postgresql",
-  :database => DATABASE_URL.path[1..-1]
+  :database => "db/development.sqlite"
 )
 
 require_all 'app'
