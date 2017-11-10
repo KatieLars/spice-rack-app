a54528c20440740b97328d07331ddbad51dcb6c1
github username <github email address>
2017-11-09 21:34:12 -0600

    Changed adapter and gem to sqlite

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Bundler.require
 #DATABASE_URL=URI.parse("postgres://ahddyelhqcrghk:a9890d2747b5a132ba2f80cf891b6e851f6c0113d9f24e65abdf0ad998940b73@ec2-50-17-235-5.compute-1.amazonaws.com:5432/d3n9tu2hps02jt")
 
 ActiveRecord::Base.establish_connection(
-  :adapter => "postgresql",
+  :adapter => "sqlite3",
   :database => "db/development.sqlite"
 )
 
