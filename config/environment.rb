61c36e8a8d9db15635f232d87a1fbb93fac9a38a
github username <github email address>
2017-11-09 20:56:31 -0600

    added DATABASE_URL

--- config/environment.rb
+++ config/environment.rb
@@ -3,11 +3,11 @@
 require 'bundler/setup'
 Bundler.require
 
-db = URI.parse('postgres://localhost/spice_rack')
+#db = URI.parse('postgres://ahddyelhqcrghk:a9890d2747b5a132ba2f80cf891b6e851f6c0113d9f24e65abdf0ad998940b73@ec2-50-17-235-5.compute-1.amazonaws.com:5432/d3n9tu2hps02jt')
 
 ActiveRecord::Base.establish_connection(
   :adapter => "postgresql",
-  :database => "/Applications/Postgres.app/Contents/Versions/9.6/bin/psql" -p5432 -d "spice_rack"
+  :database => DATABASE_URL
 )
 
 require_all 'app'
